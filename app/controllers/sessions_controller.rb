30b106a76f7d471ff3e5186e00dbc64997745134
Victor <victoraweb@gmail.com>
2016-09-14 20:09:30 -0300

    type: feat, test Remember-me functionality and test;

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
+      log_in user
+      remember user
       redirect_to user
     else
       # Unlike the contents of flash, the contents of flash.now disappear
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end

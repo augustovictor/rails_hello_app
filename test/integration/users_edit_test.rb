838e2a759d1c5b8c0fc5804de8089b34b121e32e
Victor <victoraweb@gmail.com>
2016-09-15 01:07:29 -0300

    type: feat, test Redirect user if trying to edit or update other user info;

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessfull edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {
@@ -22,6 +23,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successfull edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
 
@@ -42,6 +44,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: {
+        user: {
+            name: @user.name,
+            email: @user.email
+        }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end

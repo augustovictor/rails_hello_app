a12c3833d4bcb0494c940ba20fd8f65afb7177de
Victor <victoraweb@gmail.com>
2016-09-15 00:46:02 -0300

    type: feat, test Updating user and test;

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: { minimum: 6 },
-            presence: true
+            presence: true, allow_nil: true
 
 
   class << self

24af1b28034bc7e055d3b7b3c54586be5ecdadc5
Victor <victoraweb@gmail.com>
2016-09-16 08:42:31 -0300

    type: feat, test creating micro posts from home page + tests;

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,22 @@ class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 
 	def create
+		@micropost = current_user.microposts.build(micropost_params)
+		if @micropost.save
+			flash[:success] = "Micropost created successfully!"
+			redirect_to current_user
+		else
+			flash[:fail] = "Could not create micropost"
+			render 'static_pages/home'
+		end
 	end
 
 	def destroy
+	end
+
+	private
+
+	def micropost_params
+		params.require(:micropost).permit(:content)
 	end
 end

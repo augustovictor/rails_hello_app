75f27ef61e5f3cc679dd25eab6bbad3a66401d12
Victor <victoraweb@gmail.com>
2016-09-16 08:13:57 -0300

    type: feat, tests, refactor Controller tests; Moved logged_in_user helper method to application_helper so it would be used for many controllers;

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,10 @@ Rails.application.routes.draw do
   # Signup
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+
   resources :users
+  resources :microposts, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
